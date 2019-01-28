b37d412cac01e79f8c6ec95e9dc40a99856dae0b
Mannex Pimentel <Mannex@mannexs-air.attlocal.net>
2019-01-28 12:18:45 -0500

    controllers

---
+++ controllers/creaturesController.js
@@ -0,0 +1,38 @@
+const Creature = require('../models/Creature')
+
+const creaturesController = {
+    index: (req, res) => {
+        Creature.find({})
+            .then((creatures) => {
+                res.send(creatures)
+            })
+     },
+    show: (req, res) => {
+        Creature.findById(req.params.creatureId)
+            .then((creature) => {
+                res.send(creature)
+            })
+    },
+    update: (req, res) => {
+        Creature.findByIdAndUpdate(req.params.creatureId, req.body)
+            .then((updatedCreature) => {
+                updatedCreature.save()
+                res.send(updatedCreature)
+            })
+    },
+    delete: (req, res) => {
+        Creature.findByIdAndDelete(req.params.creatureId)
+            .then(() => {
+                res.sendStatus(200)
+            })
+    },
+    create: (req, res) => {
+        Creature.create(req.body)
+            .then((creatures) => {
+                res.send(creatures)
+            })
+    }
+}
+
+ 
+module.exports = creaturesController
